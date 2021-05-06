1ff4a1502cdba25cbab30a487c9d3b8f903478da
nsedan <naim.alejandro.sedan@gmail.com>
2021-05-06 09:03:14 +0100

    header, incomeexpenses and balance comp added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Balance from "./components/Balance";
+import IncomeExpenses from "./components/IncomeExpenses";
 
 function App() {
   return (
-    <div className="container">
+    <div>
+      <Header />
+      <div className="container">
+        <Balance />
+        <IncomeExpenses />
+      </div>
     </div>
   );
 }
