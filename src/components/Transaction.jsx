ee659640240f1856ceb1e9da515731d2e69c7d5b
nsedan <naim.alejandro.sedan@gmail.com>
2021-05-13 11:24:53 +0100

    Add and delete features added

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
-const Transaction = ({transaction}) => {
+const Transaction = ({ transaction }) => {
+  const { deleteTransaction } = useContext(GlobalContext);
+
   const sign = transaction.amount < 0 ? "-" : "+";
+
   return (
-    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+    <li className={transaction.amount < 0 ? "minus" : "plus"}>
       {transaction.text}
       <span>
         {sign}${Math.abs(transaction.amount)}
       </span>
-      <button className="delete-btn">x</button>
+      <button
+        onClick={() => deleteTransaction(transaction.id)}
+        className="delete-btn"
+      >
+        x
+      </button>
     </li>
   );
 };
